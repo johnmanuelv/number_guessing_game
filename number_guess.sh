70355445431478f63cb1478c9006bb741b630e80
John Manuel Venasquez <johnmanuelvenasquez18@gmail.com>
2025-02-27 11:39:37 +0000

    feat: Add game logic and number validation

--- number_guess.sh
+++ number_guess.sh
@@ -22,3 +22,38 @@ else
   IFS='|' read USER_ID GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while true; do
+  read GUESS
+  
+  # Check if input is an integer
+  if ! [[ "$GUESS" =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((NUMBER_OF_GUESSES++))
+
+  if [[ $GUESS -eq $SECRET_NUMBER ]]; then
+    echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+    
+    # Update user stats in the database
+    if [[ -z $USER_INFO ]]; then
+      $PSQL "UPDATE users SET games_played = 1, best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'"
+    else
+      NEW_GAMES_PLAYED=$((GAMES_PLAYED + 1))
+      if [[ -z $BEST_GAME || $NUMBER_OF_GUESSES -lt $BEST_GAME ]]; then
+        $PSQL "UPDATE users SET best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'"
+      fi
+      $PSQL "UPDATE users SET games_played = $NEW_GAMES_PLAYED WHERE username='$USERNAME'"
+    fi
+    break
+  elif [[ $GUESS -lt $SECRET_NUMBER ]]; then
+    echo "It's higher than that, guess again:"
+  else
+    echo "It's lower than that, guess again:"
+  fi
+done
